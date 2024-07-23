c63ee95620570b99d457420ab58f559326639821
Tsubasa Kato <tsubasa@superai.online>
2024-07-23 19:58:15 +0900

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,11 @@
 <?php
-//(C)Tsubasa Kato 7/16/2024 - Made with ChatGPT (GPT-4o)
-function convertHTML5toIE3($htmlContent, $baseUrl, $basePath) {
+//(C)Tsubasa Kato - 2024/7/17 22:21PM JST - Created with help from ChatGPT (GPT-4o)
+// Enable error reporting for debugging - Off in this.
+ini_set('display_errors', 0);
+ini_set('display_startup_errors', 0);
+error_reporting(E_ALL);
+
+function convertHTML5toIE3($htmlContent, $baseUrl, $basePath, $imageDir, $webImageDir) {
     // Define the replacements for HTML5 elements and attributes
     $replacements = [
         // HTML5 tags to be replaced or removed
@@ -36,14 +41,27 @@ function convertHTML5toIE3($htmlContent, $baseUrl, $basePath) {
     $cssRegex = '/(display\s*:\s*flex\s*;|grid\s*;|flexbox\s*;)/i';
     $htmlContent = preg_replace($cssRegex, 'display: block;', $htmlContent);
 
-    // Downsize images to 640 pixels in width
-    $htmlContent = preg_replace_callback('/<img[^>]+>/i', function($matches) {
+    // Extract image tags and process images in parallel
+    $imgTags = [];
+    $htmlContent = preg_replace_callback('/<img[^>]+>/i', function($matches) use ($basePath, &$imgTags) {
         $imgTag = $matches[0];
-        // Remove existing width and height attributes
-        $imgTag = preg_replace('/(width|height)="[^"]*"/i', '', $imgTag);
-        // Add the width attribute
-        return preg_replace('/<img/i', '<img width="640"', $imgTag);
+        if (preg_match('/src="([^"]+)"/i', $imgTag, $srcMatches)) {
+            $src = $srcMatches[1];
+            if (!preg_match('/^(http|https):\/\//i', $src)) {
+                $src = rtrim($basePath, '/') . '/' . ltrim($src, '/');
+            }
+            $imgTags[] = [$imgTag, $src, $srcMatches[1]];
+        }
+        return $imgTag;
     }, $htmlContent);
+
+    // Process images in parallel using curl_multi
+    $convertedImages = processImagesInParallel($imgTags, $imageDir, $webImageDir);
+
+    // Update image tags with new src
+    foreach ($convertedImages as $originalSrc => $newSrc) {
+        $htmlContent = str_replace($originalSrc, $newSrc, $htmlContent);
+    }
 
     // Rewrite URLs to redirect through the converter with absolute paths
     $htmlContent = preg_replace_callback('/<a[^>]+href="([^"]+)"[^>]*>/i', function($matches) use ($baseUrl, $basePath) {
@@ -60,41 +78,161 @@ function convertHTML5toIE3($htmlContent, $baseUrl, $basePath) {
     return $htmlContent;
 }
 
+function processImagesInParallel($imgTags, $imageDir, $webImageDir) {
+    $mh = curl_multi_init();
+    $chArray = [];
+    $convertedImages = [];
+
+    foreach ($imgTags as $imgData) {
+        [$imgTag, $src, $originalSrc] = $imgData;
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $src);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_multi_add_handle($mh, $ch);
+        $chArray[] = ['handle' => $ch, 'originalSrc' => $originalSrc];
+    }
+
+    $running = null;
+    do {
+        curl_multi_exec($mh, $running);
+        curl_multi_select($mh);
+    } while ($running > 0);
+
+    foreach ($chArray as $chData) {
+        $ch = $chData['handle'];
+        $originalSrc = $chData['originalSrc'];
+        $imageContent = curl_multi_getcontent($ch);
+        if ($imageContent !== false) {
+            $newSrc = resizeAndConvertImageContent($imageContent, $imageDir, $webImageDir);
+            if ($newSrc !== false) {
+                $convertedImages[$originalSrc] = $newSrc;
+            }
+        }
+        curl_multi_remove_handle($mh, $ch);
+        curl_close($ch);
+    }
+
+    curl_multi_close($mh);
+    return $convertedImages;
+}
+
+function resizeAndConvertImageContent($imageContent, $imageDir, $webImageDir) {
+    // Create an image resource from the content
+    $image = imagecreatefromstring($imageContent);
+    if ($image === false) {
+        return false;
+    }
+
+    $width = imagesx($image);
+    $height = imagesy($image);
+
+    // Resize if the width is over 640 pixels
+    if ($width > 640) {
+        $newWidth = $width * 0.2;
+        $newHeight = $height * 0.2;
+    } else {
+        $newWidth = $width;
+        $newHeight = $height;
+    }
+
+    $resizedImage = imagecreatetruecolor($newWidth, $newHeight);
+    imagecopyresampled($resizedImage, $image, 0, 0, 0, 0, $newWidth, $newHeight, $width, $height);
+
+    // Create a unique file name for the resized image, convert to .jpg
+    $uniqueFileName = uniqid('img_', true) . '.jpg';
+    $imageFileName = $imageDir . '/' . $uniqueFileName;
+    $webImagePath = $webImageDir . '/' . $uniqueFileName;
+    imagejpeg($resizedImage, $imageFileName, 90);
+
+    imagedestroy($image);
+    imagedestroy($resizedImage);
+
+    // Return the web path to the resized image
+    return $webImagePath;
+}
+
 // Function to check if a string is URL encoded
 function isUrlEncoded($string) {
     $string = str_replace('%20', '+', $string);
     return urldecode($string) !== $string;
 }
 
+// Function to fetch HTML content using CURL
+function fetchHtmlContentInParallel($urls) {
+    $mh = curl_multi_init();
+    $chArray = [];
+    $htmlContents = [];
+
+    foreach ($urls as $url) {
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $url);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+        curl_multi_add_handle($mh, $ch);
+        $chArray[] = $ch;
+    }
+
+    $running = null;
+    do {
+        curl_multi_exec($mh, $running);
+        curl_multi_select($mh);
+    } while ($running > 0);
+
+    foreach ($chArray as $ch) {
+        $htmlContent = curl_multi_getcontent($ch);
+        $htmlContents[] = $htmlContent;
+        curl_multi_remove_handle($mh, $ch);
+        curl_close($ch);
+    }
+
+    curl_multi_close($mh);
+    return $htmlContents;
+}
+
 // Check if the URL parameter is set
 if (isset($_GET['url'])) {
-    $url = $_GET['url'];
+    $urls = is_array($_GET['url']) ? $_GET['url'] : [$_GET['url']];
 
-    // Check if the URL is encoded and decode if necessary
-    if (isUrlEncoded($url)) {
-        $url = urldecode($url);
+    // Decode URLs if necessary
+    foreach ($urls as &$url) {
+        if (isUrlEncoded($url)) {
+            $url = urldecode($url);
+        }
     }
 
     $baseUrl = 'http://' . $_SERVER['HTTP_HOST'] . $_SERVER['PHP_SELF'];
 
-    // Fetch the HTML content from the given URL
-    $htmlContent = file_get_contents($url);
+    // Create image directory
+    $date = date('Y-m-d');
+    $imageDir = '/var/www/html/retrobrowse/img_' . $date;
+    $webImageDir = 'img_' . $date;
+    if (!file_exists($imageDir)) {
+        if (!mkdir($imageDir, 0777, true)) {
+            die('Failed to create image directory');
+        }
+    }
 
-    if ($htmlContent !== false) {
-        // Get the base path of the URL to handle relative links
-        $parsedUrl = parse_url($url);
-        $basePath = $parsedUrl['scheme'] . '://' . $parsedUrl['host'] . dirname($parsedUrl['path']) . '/';
+    // Fetch the HTML content from the given URLs using CURL in parallel
+    $htmlContents = fetchHtmlContentInParallel($urls);
 
-        // Convert the HTML content
-        $convertedContent = convertHTML5toIE3($htmlContent, $baseUrl, $basePath);
+    foreach ($htmlContents as $htmlContent) {
+        if ($htmlContent !== false) {
+            // Get the base path of the URL to handle relative links
+            $parsedUrl = parse_url($url);
+            $basePath = $parsedUrl['scheme'] . '://' . $parsedUrl['host'] . (isset($parsedUrl['path']) ? dirname($parsedUrl['path']) : '') . '/';
 
-        // Display the converted content
-        header('Content-Type: text/html; charset=UTF-8');
-        echo $convertedContent;
-    } else {
-        echo "Failed to fetch content from the URL.";
+            // Convert the HTML content
+            $convertedContent = convertHTML5toIE3($htmlContent, $baseUrl, $basePath, $imageDir, $webImageDir);
+
+            // Display the converted content
+            header('Content-Type: text/html; charset=UTF-8');
+            echo $convertedContent;
+        } else {
+            echo "Failed to fetch content from the URL.<br>";
+        }
     }
 } else {
-    echo "No URL parameter provided.";
+    echo "No URL parameter provided.<br>";
 }
 ?>
